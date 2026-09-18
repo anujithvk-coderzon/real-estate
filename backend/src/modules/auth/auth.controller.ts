a9726e1e900b2766b14f4403ee9619945ec5d5ea
Anujith vk <anujith.vk@coderzon.com>
2026-09-18 17:40:06 +0530

    (feat/google): google signup cookie expiration time correction

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -150,7 +150,7 @@ export const googleCallback=async(req:Request,res:Response)=>{
     res.cookie("refresh_real_estate",response.refreshToken,{
       httpOnly:true,
       maxAge:7*24*60*60*1000,
-      sameSite:'strict',
+      sameSite:process.env.NODE_ENV==="production" ? 'none':'lax',
       secure:process.env.NODE_ENV==="production",
     })
     res.redirect(process.env.FRONTEND_URL as string);
